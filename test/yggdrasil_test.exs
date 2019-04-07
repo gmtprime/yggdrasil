aa3eea0fbcba03acff25c292562ce14b4a7b69d7
Alexander de Sousa <alexander.jose.de.sousa@gmail.com>
2019-04-07 10:37:53 +0200

    Added tests for bridge adapter

--- test/yggdrasil_test.exs
+++ test/yggdrasil_test.exs
@@ -1,16 +1,35 @@
 defmodule YggdrasilTest do
   use ExUnit.Case, async: true
 
-  test "full subscription cycle" do
-    {:ok, channel} = Yggdrasil.gen_channel(name: make_ref())
+  describe "full subscription cycle" do
+    test ":elixir adapter" do
+      {:ok, channel} = Yggdrasil.gen_channel(name: make_ref())
 
-    assert :ok = Yggdrasil.subscribe(channel)
-    assert_receive {:Y_CONNECTED, ^channel}
+      assert :ok = Yggdrasil.subscribe(channel)
+      assert_receive {:Y_CONNECTED, ^channel}
 
-    assert :ok = Yggdrasil.publish(channel, "message")
-    assert_receive {:Y_EVENT, ^channel, "message"}
+      assert :ok = Yggdrasil.publish(channel, "message")
+      assert_receive {:Y_EVENT, ^channel, "message"}
 
-    assert :ok = Yggdrasil.unsubscribe(channel)
-    assert_receive {:Y_DISCONNECTED, ^channel}
+      assert :ok = Yggdrasil.unsubscribe(channel)
+      assert_receive {:Y_DISCONNECTED, ^channel}
+    end
+
+    test ":bridge adapter" do
+      remote = [name: make_ref()]
+      local = [name: remote, adapter: :bridge]
+
+      assert :ok = Yggdrasil.subscribe(local)
+      assert_receive {:Y_CONNECTED, _}
+
+      assert :ok = Yggdrasil.publish(local, "message")
+      assert_receive {:Y_EVENT, _, "message"}
+
+      assert :ok = Yggdrasil.publish(remote, "message")
+      assert_receive {:Y_EVENT, _, "message"}
+
+      assert :ok = Yggdrasil.unsubscribe(local)
+      assert_receive {:Y_DISCONNECTED, _}
+    end
   end
 end
